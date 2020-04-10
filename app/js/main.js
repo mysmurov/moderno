ae487ed701f3227491d0482b26d1439accc93c9e
mysmurov <mysmurov@gmail.com>
2020-04-10 20:52:28 +0300

    add product_slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
-$(function(){
+$(function () {
+
+  $(".rate-star").rateYo({
+    rating: 5.0,
+    starWidth: "12px",
+    readOnly: true,
+  });
+
+  $(".product-slider__inner").slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+  });
 
   var mixer = mixitup('.products__inner-box');
-  
+
 });
